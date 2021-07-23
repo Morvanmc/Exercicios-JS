fcb833eb5d1b16698d3ba942057598b8b0d83434
Morvanmc <morvanmc@outlook.com>
2021-07-23 16:22:27 -0300

    Atualização DropDown

---
+++ js/funcoesComplementares.js
@@ -0,0 +1,5 @@
+/* Mostrar e esconder a lista de exercícios */
+function dropDown() {
+    let list = document.getElementsByClassName('list-item');
+    list.style.display = "none";
+}
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
+const bodyParser = require('body-parser') //Instância do body-parse
+const express = require('express') //Instância do express
+const app = express() //Atribuindo o express a uma const app
 
-app.use(express.static('.'))
+app.use(express.static('.')) //Garante o express ao arquivos estáticos no projeto
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.listen(8080, () => console.log('Executando...'))
+app.listen(8080, () => console.log('Executando...')) //Porta onde o servidor está sendo executado
