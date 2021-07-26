aa722729d9489a7938df9f6635f1c0d1c90ae0bf
Morvanmc <morvanmc@outlook.com>
2021-07-26 16:21:07 -0300

    Update Home e Add o Button show Code

--- js/exercicios.js
+++ js/exercicios.js
@@ -2,14 +2,14 @@
 
 function cumprimentar() {
     const name = document.getElementById('name-c1').value;
-    document.getElementById('resultado-c1').innerHTML = `Olá, ${name}!`;
+    document.getElementById('resultado').innerHTML = `Olá, ${name}!`;
 }
 
 /* Exercício 02 */
 
 function anoEmDias() {
     const age = document.getElementById('age-c2').value;
-    document.getElementById('resultado-c2').innerHTML = `Idade em dias = ${age * 365}`
+    document.getElementById('resultado').innerHTML = `Idade em dias = ${age * 365}`
 }
 
 /* Exercício 03 */
@@ -18,7 +18,7 @@ function calcularSalario() {
     const qtdHoras = document.getElementById('qtdHoras-c3').value;
     const vlrHoras = document.getElementById('vlrHoras-c3').value;
 
-    document.getElementById('resultado-c3').innerHTML = `Salário igual a R$ ${qtdHoras * vlrHoras}`
+    document.getElementById('resultado').innerHTML = `Salário igual a R$ ${qtdHoras * vlrHoras}`
 }
 
 /* Exercício 04 */
@@ -28,43 +28,43 @@ function mesPorNumero() {
 
     switch(num) {
         case 1: 
-            document.getElementById('resultado-c4').innerHTML = 'Janeiro'
+            document.getElementById('resultado').innerHTML = 'Janeiro'
             break
         case 2: 
-            document.getElementById('resultado-c4').innerHTML = 'Fevereiro'
+            document.getElementById('resultado').innerHTML = 'Fevereiro'
             break
         case 3: 
-            document.getElementById('resultado-c4').innerHTML = 'Março'
+            document.getElementById('resultado').innerHTML = 'Março'
             break;
         case 4: 
-            document.getElementById('resultado-c4').innerHTML = 'Abril'
+            document.getElementById('resultado').innerHTML = 'Abril'
             break
         case 5: 
-            document.getElementById('resultado-c4').innerHTML = 'Maio'
+            document.getElementById('resultado').innerHTML = 'Maio'
             break
         case 6: 
-            document.getElementById('resultado-c4').innerHTML = 'Junho'
+            document.getElementById('resultado').innerHTML = 'Junho'
             break
         case 7: 
-            document.getElementById('resultado-c4').innerHTML = 'Julho'
+            document.getElementById('resultado').innerHTML = 'Julho'
             break
         case 8: 
-            document.getElementById('resultado-c4').innerHTML = 'Agosto'
+            document.getElementById('resultado').innerHTML = 'Agosto'
             break
         case 9: 
-            document.getElementById('resultado-c4').innerHTML = 'Setembro'
+            document.getElementById('resultado').innerHTML = 'Setembro'
             break
         case 10: 
-            document.getElementById('resultado-c4').innerHTML = 'Outubro'
+            document.getElementById('resultado').innerHTML = 'Outubro'
             break
         case 11: 
-            document.getElementById('resultado-c4').innerHTML = 'Novembro'
+            document.getElementById('resultado').innerHTML = 'Novembro'
             break
         case 12: 
-            document.getElementById('resultado-c4').innerHTML = 'Dezembro'
+            document.getElementById('resultado').innerHTML = 'Dezembro'
             break
         default: 
-            document.getElementById('resultado-c4').innerHTML = 'Mês inválido!'
+            document.getElementById('resultado').innerHTML = 'Mês inválido!'
             break
     }
 }
@@ -76,8 +76,8 @@ function trueOrFalse() {
     const num2 = parseInt(document.getElementById('SegundoNum-c5').value);
 
     if(typeof num1 !== typeof num2) {
-        document.getElementById('resultado-c5').innerHTML = false;
+        document.getElementById('resultado').innerHTML = false;
     } else {
-        document.getElementById('resultado-c5').innerHTML = `${num1 >= num2}`;
+        document.getElementById('resultado').innerHTML = `${num1 >= num2}`;
     }
 }
--- js/funcoesComplementares.js
+++ js/funcoesComplementares.js
@@ -1,5 +1,17 @@
 /* Mostrar e esconder a lista de exercícios */
 function dropDown() {
-    let list = document.querySelector('.list-itens');
+    const list = document.querySelector('.list-itens');
     list.classList.toggle('hidden');
+}
+
+/*Esconde o conteúdo da div apresentação na Section*/
+function hiddenApresentacao() {
+    const home = document.querySelector('.apresentacao');
+    home.classList.add('hidden');
+}
+
+/* Esconde o Código */
+function hiddenCode() {
+    const code = document.querySelector('.code-box');
+    code.classList.toggle('hidden');
 }
