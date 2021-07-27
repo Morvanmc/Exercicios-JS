74ae3ec9ab51b472213b7e557173a0e1995ef76f
Morvanmc <morvanmc@outlook.com>
2021-07-27 16:54:24 -0300

    Novos Exercícios

--- js/exercicios.js
+++ js/exercicios.js
@@ -81,3 +81,76 @@ function trueOrFalse() {
         document.getElementById('resultado').innerHTML = `${num1 >= num2}`;
     }
 }
+
+/* Exercício 06 */
+
+function inverso() {
+    const valor = document.getElementById('valor-c6').value
+    if(valor == 'true' || valor == 'false') {
+        switch(valor) {
+            case 'true':
+                document.getElementById('resultado').innerHTML = false
+                break
+            case 'false':
+                document.getElementById('resultado').innerHTML = true
+                break
+        }
+    } else {
+        const valorN = parseFloat(valor)
+        typeof valorN === 'number' && !isNaN(valorN) ? 
+        document.getElementById('resultado').innerHTML = `${-valorN}` : 
+        document.getElementById('resultado').innerHTML = `Valor Boolean ou Number esperado!`
+    }  
+}
+
+/* Exercício 07 */
+
+function estaEntre() {
+    const num = parseFloat(document.getElementById('num-c7').value);
+    const min = parseFloat(document.getElementById('min-c7').value);
+    const max = parseFloat(document.getElementById('max-c7').value);
+    const inclusivo = document.getElementById('inclusivo-c7').value;
+
+    inclusivo == 'false' ? document.getElementById('resultado').innerHTML = `${num > min && num < max}` : 
+    document.getElementById('resultado').innerHTML = `${num >= min && num <= max}`
+}
+
+/* Exercício 08 */
+
+function multiplicar() {
+    const num1 = parseFloat(document.getElementById('num1-c8').value);
+    const num2 = parseFloat(document.getElementById('num2-c8').value);
+
+    if(Math.sign(num1) == -1 || Math.sign(num2) == -1) {
+        document.getElementById('resultado').innerHTML = 'Não são aceito números negativos!'
+    } else if( num1 == 0 || num2 == 0) {
+        document.getElementById('resultado').innerHTML = 0
+    } else {
+        let resultado = 0
+        for(let i = 0; i < num1; i++) {
+            resultado += num2;
+        }
+        document.getElementById('resultado').innerHTML = `${resultado}`
+    }
+}
+
+/* Exercício 09 */
+function repetir() {
+    const param = document.getElementById('param-c9').value;
+    const num = parseInt(document.getElementById('num-c9').value);
+    result = []
+    for(let i = 0; i < num; i++) {
+        result.push(param)
+    }
+    document.getElementById('resultado').innerHTML = `${result}`
+}
+
+/* Exercício 10 */
+function repetirPlus() {
+    const num = parseInt(document.getElementById('num-c10').value);
+    result = ''
+    for(let i = 0; i < num; i++) {
+        result += '+'
+    }
+    document.getElementById('resultado').innerHTML = `${result}`
+}
